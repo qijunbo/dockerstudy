59cf1f7f25361e3b9ab4e15189edd24b075cf1f1
qijunbo <junboqi@foxmail.com>
2017-09-29 16:26:20 +0800

    a better version of verify.sh

--- officalMysql/verify.sh
+++ officalMysql/verify.sh
@@ -7,8 +7,13 @@ if [ -z "${name}" ]; then
 fi
 
 docker exec -it ${name} mysqladmin -uroot -psunway123# variables  | grep  "case"
-
+echo 
+echo "-------------------------------"
+echo "Customized mysql configure: "
 docker exec -it ${name} cat /etc/my.cnf 
+echo "-------------------------------"
+echo "check the existance of init.sql :" 
+docker exec -it ${name} ls -l /docker-entrypoint-initdb.d
 echo 
 docker exec -it ${name} mysql -uroot -psunway123# -e "show databases;"
 
