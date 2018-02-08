51e49f6a8ed1f4b3eea7b24cd6a59d662a7324d3
DESKTOP-QECEDFM\qijunbo <junboqi@foxmail.com>
2018-02-08 23:35:16 +0800

    move data using temp table.

---
+++ mysql/use_temp.sql
@@ -0,0 +1,31 @@
+SELECT NSID, NSNAME, NSYEAR, NSCODE, NSDATE, COMMENTS, `TYPE`, RISKLEVEL, PNSID, OLDID
+FROM lims_st.st_inspection;
+
+SELECT ID, NSID, STATUS, TYPE1, TYPE2, TYPE3, TYPE4, RISKLEVEL, COMMENTS
+FROM lims_st.st_temp_inspectiondetail limit 10;
+
+delete    FROM lims_st.st_temp_inspection;
+
+-- level 1
+insert into lims_st.st_temp_inspection ( NSID, NSNAME, PNSID, TYPE , OLDID)
+select  uuid() as NSID,   TYPE1 as NSNAME , NSID as PNSID, 1 as  `TYPE`,  max( ID ) as OLDID 
+FROM lims_st.st_inspectiondetail  group by  TYPE1 , NSID
+
+insert into lims_st.st_temp_inspection ( NSID, NSNAME, PNSID, TYPE , OLDID)
+select  uuid() as NSID, d.TYPE2 as NSNAME , t.nsid as PNSID, 2 as `TYPE`,  max( ID ) as OLDID
+FROM lims_st.st_temp_inspectiondetail as d left join lims_st.st_temp_inspection t on d.TYPE1 = t.NSNAME
+where t.`TYPE` = 1
+group by t.nsid , d.TYPE2 
+
+insert into lims_st.st_temp_inspection ( NSID, NSNAME, PNSID, TYPE , OLDID)
+select  uuid() as NSID, d.TYPE3 as NSNAME , t.nsid as PNSID, 3 as `TYPE`,  max( ID ) as OLDID
+FROM lims_st.st_temp_inspectiondetail as d left join lims_st.st_temp_inspection t on d.TYPE2 = t.NSNAME
+where t.`TYPE` = 2
+group by t.nsid , d.TYPE3 
+
+insert into lims_st.st_temp_inspection ( NSID, NSNAME, PNSID, TYPE , OLDID)
+select  ID as NSID, d.TYPE4 as NSNAME , t.nsid as PNSID, 4 as `TYPE`,  ID as OLDID
+FROM lims_st.st_temp_inspectiondetail as d left join lims_st.st_temp_inspection t on d.TYPE3 = t.NSNAME
+where t.`TYPE` = 3
+
+SELECT * FROM lims_st.st_temp_inspection;
