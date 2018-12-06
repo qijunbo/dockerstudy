2405eb919714f834f2672efdcbe3574b5ff8121f
TonyAli <junboqi@foxmail.com>
2018-12-06 16:57:41 +0800

    change upload.sh

--- java8/upload.sh
+++ java8/upload.sh
@@ -5,7 +5,10 @@ if [ -z "${version}" ]; then
    exit 1
 fi
 
-docker login --username=junboqi@foxmail.com registry.cn-hangzhou.aliyuncs.com
-docker tag qijunbo/jdk:${version} registry.cn-hangzhou.aliyuncs.com/qijunbo/jdk:${version}
-nohup docker push registry.cn-hangzhou.aliyuncs.com/qijunbo/jdk:${version} &
+#docker login --username=junboqi@foxmail.com registry.cn-hangzhou.aliyuncs.com
+#docker tag qijunbo/jdk:${version} registry.cn-hangzhou.aliyuncs.com/qijunbo/jdk:${version}
+#nohup docker push registry.cn-hangzhou.aliyuncs.com/qijunbo/jdk:${version} &
 
+docker login --username=junboqi@foxmail.com registry.cn-qingdao.aliyuncs.com
+docker tag  qijunbo/jdk:${version}  registry.cn-qingdao.aliyuncs.com/qijunbo/jdk:8
+nohup docker push registry.cn-qingdao.aliyuncs.com/qijunbo/jdk:8 &
