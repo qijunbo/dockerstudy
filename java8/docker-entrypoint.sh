f086deb94995c2cc8c3f1c8fde6b3569ffeaf1db
TonyAli <junboqi@foxmail.com>
2018-12-26 14:04:08 +0800

    refine docker-entrypoint.sh

--- java8/docker-entrypoint.sh
+++ java8/docker-entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 #set -e
-echo `ls *jar`
-jarfile=`ls *jar`
-java  $JAVA_OPTS -jar  ${jarfile}
+
+
+if [ -x "/init.d/start.sh" ]; then
+    echo `ls -lh /init.d`
+    /init.d/start.sh
+
+elif  [ -x "$jarfile" ]; then
+    jarfile=`ls *jar`
+    echo $jarfile
+    java  $JAVA_OPTS -jar  ${jarfile}
+fi
+
+echo '已经执行完所有指令, 进入空转程序.'
+while sleep 60; do
+
+  echo `ps -aux`
+
+done
 
  
